ba4c36bd20e41dab8c98051e89da78ef7e219791
Stephen Whitmore <sww@eight45.net>
2017-09-08 18:31:23 -0400

    test: fix tests

--- test/adder.js
+++ test/adder.js
@@ -13,11 +13,43 @@ test('adder', function (t) {
 
   var idx = index(db, {
     processFn: function (kv, _, next) {
+      console.log('pro', kv)
       if (typeof kv.value === 'number') sum += kv.value
       next()
+
+      if(!--pending) done()
     },
     getSnapshot: function (cb) { cb(null, snapshot) },
     setSnapshot: function (s, cb) { snapshot = s; cb(null) }
+  })
+
+  var pending = 3
+  db.put('/foo/bar', 17, function (err) { t.error(err) })
+  db.put('/foo/baz', 12, function (err) { t.error(err) })
+  db.put('/bax/12', 1, function (err) { t.error(err) })
+
+  function done () {
+    idx.ready(function () {
+      t.equal(sum, 30)
+    })
+  }
+})
+
+test.skip('adder /w slow snapshots', function (t) {
+  t.plan(4)
+
+  var db = hyperdb(ram, { valueEncoding: 'json' })
+
+  var sum = 0
+  var snapshot = null
+
+  var idx = index(db, {
+    processFn: function (kv, _, next) {
+      if (typeof kv.value === 'number') sum += kv.value
+      next()
+    },
+    getSnapshot: function (cb) { setTimeout(function () { cb(null, snapshot) }, 100) },
+    setSnapshot: function (s, cb) { snapshot = s; setTimeout(cb, 500) }
   })
 
   var pending = 3
@@ -29,7 +61,7 @@ test('adder', function (t) {
     t.error(err)
     if (!--pending) {
       idx.ready(function () {
-        t.equal(sum, 29)
+        t.equal(sum, 30)
       })
     }
   }
